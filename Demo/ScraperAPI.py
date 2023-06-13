0a0ed7b6fbd02a60a1696fb4744bd24996bd3f8e
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2023-06-13 19:47:54 +0700

    Demo 3 test Usable
    Scraper API and Facebook Scraper is Work perfectly fine but Tweepy is kinda work but meh

--- Demo/ScraperAPI.py
+++ Demo/ScraperAPI.py
@@ -1,9 +1,26 @@
 import requests
-url = 'https://www.google.com'
-api_key = ""
-base_url = "https://api.scraperapi.com?api_key="+api_key+"&url="
-request_url = base_url + url
-response = requests.get(request_url)
-content = response.text
+import pandas as pd
 
-print(content)
+twitter_data = []
+
+payload = {
+    'api_key': '',
+    'query': 'sentiment analysis',
+    'num': '100'
+}
+response = requests.get(
+    'https://api.scraperapi.com/structured/twitter/search', params=payload)
+data = response.json()
+
+all_tweets = data['tweets']
+for tweet in all_tweets:
+    twitter_data.append({
+        'ID': tweet['tweet_id'],
+        'User': tweet["user"],
+        'Tweet': tweet["text"],
+        'URL': tweet["link"]
+    })
+
+df = pd.DataFrame(twitter_data)
+df.to_json('scraped_tweets.json', orient='index')
+print('Tweets exported to JSON')

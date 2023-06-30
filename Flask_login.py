8a09553f6c82c3bc67e43ca967e3484738306a19
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2023-06-30 14:26:09 +0700

    try to add to DB can't

--- Flask_login.py
+++ Flask_login.py
@@ -1,14 +1,17 @@
 # app.py
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-
+from pymongo import MongoClient
 
 # instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['database1']
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
+
 
 
 @app.route('/ping', methods=['GET'])
@@ -21,35 +24,44 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
 
     # Replace the following code with your own authentication logic
-    if username == 'admin' and password == 'password':
+    if username == 'admin' and password == 'admin1':
+        print(username,password)
         return jsonify({'message': 'Login successful'})
     else:
+        print(username,password)
         return jsonify({'error': 'Invalid username or password'})
 
 @app.route('/register', methods=['POST'])
 def register():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    # Replace the following code with your own registration logic
-    if username and password:
+    data = request.json
+    collection = db['DB1']
+    result = collection.insert_one(data)
+    if result.acknowledged:
+        print(data)
         return jsonify({'message': 'Registration successful'})
     else:
+        print(data)
         return jsonify({'error': 'Please provide username and password'})
     
 @app.route('/userinfo')
 def yeet():
     return render_template('userinfo.html')
 
-@app.route('/test', methods=['POST'])
+
+
+@app.route('/testDB', methods=['POST'])
 def handle_data():
     data = request.get_json()
     # Access the sent data using data['key'] where 'key' is the key in the sent data object
-    received_data = data['data']
+    username = "gda"
+    password = "gda"
+    collection = db['DB1']
+    result = collection.insert_one(username,password)
     # Process the received data as needed
     # ...
 

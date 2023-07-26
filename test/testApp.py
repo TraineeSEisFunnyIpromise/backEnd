d8e6b18acff39e72e47d06015e4feea2d38253f8
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2023-07-26 10:55:21 +0700

    test not finish

--- test/testApp.py
+++ test/testApp.py
@@ -16,39 +16,29 @@ class TestLogin(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['message'], 'Login successful')
 
-    def test_invalid_login(self):
-        # Test invalid login credentials
-        response = self.app.post('/login', json={'username': 'user', 'password': 'wrongpassword'})
-        data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['error'], 'Invalid username or password')
-
-if __name__ == '__main__':
-    unittest.main()
+# class TestApp_login2():
         
-class TestApp_2():
+#     def test_deleteUser(self):
+#         # Test case for some_function_to_test
+#         result = some_function_to_test(3, 5)
+#         self.assertEqual(result, 8)
         
-    def test_deleteUser(self):
-        # Test case for some_function_to_test
-        result = some_function_to_test(3, 5)
-        self.assertEqual(result, 8)
+#     def test_resetPassword(self):
+#         # Test case for some_function_to_test
+#         result = some_function_to_test(3, 5)
+#         self.assertEqual(result, 8)
         
-    def test_resetPassword(self):
-        # Test case for some_function_to_test
-        result = some_function_to_test(3, 5)
-        self.assertEqual(result, 8)
+# class TestApp_FB():
         
-class TestApp_FB():
+#     def test_Scraped_Data_FB(self):
+#         # Test case for some_function_to_test
+#         result = some_function_to_test(3, 5)
+#         self.assertEqual(result, 8)
         
-    def test_Scraped_Data_FB(self):
-        # Test case for some_function_to_test
-        result = some_function_to_test(3, 5)
-        self.assertEqual(result, 8)
-        
-    def test_Scraped_ID_FB(self):
-        # Test case for some_function_to_test
-        result = some_function_to_test(3, 5)
-        self.assertEqual(result, 8)
+#     def test_Scraped_ID_FB(self):
+#         # Test case for some_function_to_test
+#         result = some_function_to_test(3, 5)
+#         self.assertEqual(result, 8)
 
 if __name__ == '__main__':
     unittest.main()

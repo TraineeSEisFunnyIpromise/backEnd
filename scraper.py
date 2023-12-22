163f09726db75eaf1fa10f22e3739db8abd725ea
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2023-12-22 20:28:02 +0700

    can't scrape amazon 503
    it's about legal thing which im not sure

--- scraper.py
+++ scraper.py
@@ -1,29 +1,36 @@
-# -*- coding: utf-8 -*-
 import scrapy
-from amazon.amazon.items import AmazonItem
-import chardet
+from amazon.amazon.items import AmazonItem  # Adjust path if needed
 from scrapy.crawler import CrawlerProcess
-
+#educational purpose only
 class AmazonProductSpider(scrapy.Spider):
-  name = "AmazonDeals"
-  allowed_domains = ["amazon.com"]
-  start_urls = []
-  def parse(self, response):
-   items = AmazonItem()
-   title = response.xpath('//h1[@id="title"]/span/text()').extract()
-   sale_price = response.xpath('//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()').extract()
-   category = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
-   availability = response.xpath('//div[@id="availability"]//text()').extract()
-   items['product_name'] = ''.join(title).strip()
-   items['product_sale_price'] = ''.join(sale_price).strip()
-   items['product_category'] = ','.join(map(lambda x: x.strip(), category)).strip()
-   items['product_availability'] = ''.join(availability).strip()
-   yield items
+    name = "AmazonDeals"
+    # allowed_domains = ["amazon.com"]
+    # custom_settings = {
+    # 'DOWNLOADER_MIDDLEWARES' : 
+    # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware' : None,
+    # 'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    # 'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+    # 'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,
+    # ## settings.py
+    # 'FAKEUSERAGENT_PROVIDERS' = 
+    #     'scrapy_fake_useragent.providers.FakeUserAgentProvider',  # This is the first provider we'll try
+    #     'scrapy_fake_useragent.providers.FakerProvider',  # If FakeUserAgentProvider fails, we'll use faker to generate a user-agent string for us
+    #     'scrapy_fake_useragent.providers.FixedUserAgentProvider',  # Fall back to USER_AGENT value
+    # }
 
+    def parse(self, response):
+        items = AmazonItem()
+        items['product_name'] = response.css(".a-size-medium a-color-base a-text-normal::text").get()
+        yield items
 
-def __init__(self, keyword):
-   keyword = input("Enter search keyword: ")
-   self.start_urls = [f'https://www.amazon.com/s?k={keyword}']
+        next_page = response.css("li.a-last a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def __init__(self, keyword):
+
+        self.start_urls = [f'https://www.lazada.co.th/catalog/?q={keyword}']
+
 
 def main():
     keyword = input("Enter search keyword: ")
@@ -31,10 +38,9 @@ def main():
     process.start()
 
 process = CrawlerProcess(settings={
-    'FEED_URI':'Car.csv',
-    'FEED_FORMAT':'csv'
+    'FEED_URI': 'Car.csv',
+    'FEED_FORMAT': 'csv'
 })
 
 if __name__ == '__main__':
-   main()
-   
+    main()

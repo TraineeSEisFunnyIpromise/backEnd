832728f2df6779af01778289419531dca27827df
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2025-01-11 18:42:57 +0700

    add more in search
    this kinda weird but i will check it i mean i kinda surprise too

--- tests/integrationtest/searchPageandSearchControl_test.py
+++ tests/integrationtest/searchPageandSearchControl_test.py
@@ -13,28 +13,45 @@ class TestSearchReview(unittest.TestCase):
     soup = open("raw_result_test.txt", "w+",encoding="utf-8")
     word = "electric car"
     
-    def test_success_search_product(self):
-        result = scrape_amazon("electric spoon","")
-        self.assertIsNotNone(result)
+def test_success_scrape_product(self):
+        for asin in self.asin:
+            result = scrape_amazon_product(asin)
+            self.assertIsNotNone(result)
     
-    def test_unsuccess_search_product(self):
-        result = scrape_amazon("electric spoon","")
-        self.assertIsNone(result)
-        
-    def test_success_scrape_product(self,asin):
-        result = scrape_amazon_product(asin)
-        self.assertIsNotNone(result)
-    
-    def test_unsuccess_scrape_product(self):
-        result = scrape_amazon_product([])
+def test_unsuccess_scrape_product(self):
+        result = scrape_amazon_product("")
         self.assertIsNone(result)
 
-    def test_success_scrape_product(self,word):
-        result = reqsender(word)
+def test_success_change_data(self):
+        result = reqsender(self.word)
         self.assertIsNotNone(result)
     
-    def test_unsuccess_scrape_product(self):
-        result = reqsender([])
+def test_unsuccess_change_data(self):
+        result = reqsender("")
+        self.assertIsNone(result)
+
+def test_scrape_amazon_invalid_data_type(self):
+        with self.assertRaises(TypeError):
+            scrape_amazon(12345, None)
+
+def test_scrape_amazon_product_invalid_data_type(self):
+        with self.assertRaises(TypeError):
+            scrape_amazon_product(12345)
+
+def test_change_data_invalid_data_type(self):
+        with self.assertRaises(TypeError):
+            reqsender(12345)
+
+def test_scrape_amazon_empty_search_group(self):
+        result = scrape_amazon("electric spoon", None)
+        self.assertIsNotNone(result)
+
+def test_scrape_amazon_product_empty_json_file(self):
+        result = scrape_amazon_product("DP023543", None)
+        self.assertIsNotNone(result)
+
+def test_change_data_empty_word(self):
+        result = reqsender(None)
         self.assertIsNone(result)
 
 if __name__ == '__main__':
